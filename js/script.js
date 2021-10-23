d063365a5c9a9b0c04924536e353ed83c4accf11
Michael <m.romanov.biz@gmail.com>
2021-10-23 23:40:20 +0400

    creating quest form and divider

--- js/script.js
+++ js/script.js
@@ -31,8 +31,27 @@ $(document).ready(function(){
         }, 'xml');
         });
 
-    $('.popular__quest').on('click', 'div:not(.quest__wrapper_active)', function() {
+/*     $('.left').on('click', 'div:not(.quest__wrapper_active)', function() {
         $(this)
             .addClass('quest__wrapper_active').siblings().removeClass('quest__wrapper_active')
-        });
+        }); */
+
+        var acc = document.getElementsByClassName("quest__border");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("quest__border_active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
   });
