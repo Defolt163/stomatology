554d5c09af0c3b7a922fa428603e3e4d75c95489
Michael <m.romanov.biz@gmail.com>
2021-10-25 04:08:16 +0400

    adaptation complete, add humburger

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
             prevArrow: '<button type="button" class="slick-prev"><img src="ico/arrows/left.svg"></button>',
             responsive: [
             {
-                breakpoint: 992,
+                breakpoint: 993,
                 settings: {
                 arrows: false,
                 dots: false,
@@ -80,3 +80,21 @@ $(document).ready(function(){
 
         $('input[name=phone]').mask("+7 (999) 999 99-99");
   });
+
+  window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu'),
+    menuItem = document.querySelectorAll('.menu_item'),
+    hamburger = document.querySelector('.humburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('humburger_active');
+        menu.classList.toggle('menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('humburger_active');
+            menu.classList.toggle('menu_active');
+        })
+    })
+})
